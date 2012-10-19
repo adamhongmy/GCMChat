3bb186c8c481ba075ca99b69a4453e4d53ab2f73
Costas Mantratzis <costas@frojd.se>
2012-10-19 11:57:33 +0200

    Layout UI

--- gen/se/frojd/gcmchat/R.java
+++ gen/se/frojd/gcmchat/R.java
@@ -14,13 +14,27 @@ public final class R {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
     }
+    public static final class id {
+        public static final int ll_chat_screen=0x7f070000;
+        public static final int message=0x7f070003;
+        public static final int read_more_button=0x7f070004;
+        public static final int recipients_nickname=0x7f070002;
+        public static final int sender_nickname=0x7f070001;
+    }
     public static final class integer {
-        public static final int samle_integer_var=0x7f030000;
+        public static final int samle_integer_var=0x7f040000;
+    }
+    public static final class layout {
+        public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int app_name=0x7f050000;
+        public static final int button_caption_send_message=0x7f050004;
+        public static final int hint_recipient_nickname=0x7f050002;
+        public static final int hint_type_message=0x7f050003;
+        public static final int hint_your_nickname=0x7f050001;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
