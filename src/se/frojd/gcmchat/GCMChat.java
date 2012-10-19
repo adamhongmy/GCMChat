2bd6ffc9c6a0e270d31ed79eeef2bd19c6b976e1
Costas Mantratzis <costas@frojd.se>
2012-10-19 13:47:44 +0200

    Button added

--- src/se/frojd/gcmchat/GCMChat.java
+++ src/se/frojd/gcmchat/GCMChat.java
@@ -3,6 +3,7 @@ package se.frojd.gcmchat;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gcm.GCMRegistrar;
@@ -13,6 +14,7 @@ public class GCMChat extends Activity {
 	private TextView tv_sender_nickname;
 	private TextView tv_recipients_nickname;
 	private TextView tv_message;
+	private Button btn_read_more_button;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,7 @@ public class GCMChat extends Activity {
 		tv_sender_nickname = (TextView) findViewById(R.id.sender_nickname);
 		tv_recipients_nickname = (TextView) findViewById(R.id.recipients_nickname);
 		tv_message = (TextView) findViewById(R.id.message);
+		btn_read_more_button = (Button) findViewById(R.id.read_more_button);
 		
 		final String regId = GCMRegistrar.getRegistrationId(this);
 		Log.i(debugAppTag, "registration id =====  "+regId);
